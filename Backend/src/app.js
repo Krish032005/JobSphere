6fab1941c432751d910bcf1159434121ce3c0b09
Krish Mehta <krish.mehta.20053@gmail.com>
2026-08-03 00:22:12 +0530

    register&login api

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
 
+const authRouter = require("../src/routes/auth.route");
+
+app.use(express.json());
+
+app.use("/api/auth/", authRouter);
+
+
 
 module.exports= app;
---
+++ Backend/src/controllers/auth.controller.js
@@ -0,0 +1,79 @@
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+
+async function registerUser(req,res){
+    const { name, email, password, role="candidate"} = req.body;
+
+    if (!name || !email || !password) {
+    return res.status(400).json({
+        message: "All fields are required"
+    });
+    }
+
+   const isUserAlreadyExists = await userModel.findOne({email});
+
+   if(isUserAlreadyExists){
+    return res.status(409).json({ message : "User Already Exists !!"});
+   }
+
+   const hash =  await bcrypt.hash(password,10);
+
+    const user = await userModel.create({
+    name,
+    email : email.toLowerCase(),
+    password : hash,
+    role,
+   })
+   const token = jwt.sign({
+    id : user._id,
+    role : user.role,
+   }, process.env.JWT_SECRET);
+
+   res.cookie("token",token);
+
+   res.status(200).json({
+    message : "User created successfully !!",
+    user,
+   })
+}
+
+
+async function loginUser(req,res){
+    try{
+
+        const { email, password} = req.body;
+    
+        const User = await userModel.findOne({email : email.toLowerCase()});
+    
+        if(!User){
+            return res.status(401).json({ message : "Unauthorized !!"});
+        }
+    
+        const isPasswordCorrect = await bcrypt.compare(password, User.password);
+        if(!isPasswordCorrect){
+            return res.status(409).json({message : "Invalid Credentials"})
+        }
+
+        const token = jwt.sign({
+            id : User._id,
+            role : User.role
+        }, process.env.JWT_SECRET);
+
+        res.cookie("token", token)
+        
+        res.status(200).json({
+            message : "Login Successfully !!",
+            User
+        })
+    }catch(err){
+        console.log(err);
+        res.status(409).json({
+            message : "Invalid Credentials"
+        })
+    }
+}
+
+
+module.exports= {registerUser, loginUser};
